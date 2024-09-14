a93ed43d17990291e26900c2186792f6dbce9d17
JuanVCPeru <juanvelascocolan@gmail.com>
2024-09-14 09:35:07 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -8,10 +8,10 @@ from ramachandraw.utils import plot
 st.title("Generador de Diagrama de Ramachandran")
 st.text("Autor: Juan Velasco - trucho")
 
-pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "3PL1")
+pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "1AO6")
 pdb_file = fetch_pdb(pdb_id)
 
 plt.figure()
 plot(pdb_file)
-st.markdown("Resultado :gift:")
+st.markdown("Resultado :clown_face:")
 st.pyplot()
